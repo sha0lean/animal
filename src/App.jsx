21f73f9e85b2dac098e0e77d15653fe8198894ff
shao <ligeard.pro@gmail.com>
2023-11-23 02:26:53 -0500

    💄 learn & make tailwind work in a safe space

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import Layout from "./layout.jsx"
 function App() {
   return (
     <Layout>
-      <p className="text-xl font-geist">Animal Lodge Bar</p>
-      <p className="text-xl">Animal Lodge Bar</p>
-      <p className="text-xl font-geist">ANIMAL LODGE BAR</p>
-      <p className="text-xl">ANIMAL LODGE BAR</p>
+      <div className="pt-10 w-full text-center">
+        <p className="text-5xl font-geist">Animal Lodge Bar</p>
+        <br />
+        <p className="text-5xl font-geist">ANIMAL LODGE BAR</p>
+        <br />
+        <p className="text-4xl font-heading">heading title style</p>
+        <br />
+        <p className="text-2xl font-body">body style Lorem Ipsum dela forte</p>
+      </div>
     </Layout>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // prettier-ignore
   theme: {
+    // redéfinir complètement une propriété
+    screens: {
+      sm:   "480px",
+      md:   "768px",
+      lg:   "976px",
+      xl:   "1440px",
+    },
+    colors: {
+      // bg-"alias" || text-"alias" || border-"alias" || etc...
+      blue:        "#1fb6ff",
+      pink:        "#EA80FC",
+      green:       "#13ce66",
+      gray:        "#8492a6",
+      graylight:   "#d3dce6",
+      graydark:    "#273444",
+    },
+
     extend: {
+      // ajouter ou modifier légèrement les paramètres par défaut.
+      backgroundColor: {
+        // bg-"alias"
+        primary:     "#292929",
+        secondary:   "#fff0bf",
+      },
+      textColor: {
+        // text-"alias"
+        primary:     "#f3f3f3",
+        secondary:   "#000000",
+      },
       fontFamily: {
-        geist: ["Geist", "sans-serif"],
+        // font-"alias"
+        body:      ["Open Sans",  "sans-serif"],
+        heading:   ["Montserrat", "sans-serif"],
+        geist:     ["Geist",      "sans-serif"],
       },
     },
     plugins: [],
