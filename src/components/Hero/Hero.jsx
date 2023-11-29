e6ac99b163a9007c88da74517597d434bc055905
shao <ligeard.pro@gmail.com>
2023-11-28 20:02:42 -0500

    🍱 introduce cta in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,26 @@
-// Hero.jsx
+//=components/hero/Hero.jsx
+
 import logoAnimal from "../../assets/logo/logo_animal_white.png"
+import CallToAction from "../ui/CallToAction.jsx"
 import styles from "./hero.module.css"
 
 const Hero = () => {
+  console.log(styles)
   return (
     <div
       className={
-        styles.hero +
-        " flex flex-col items-start justify-around bg-contain bg-right-top md:flex-row"
+        styles.hero + " hero-section flex flex-col items-start justify-around bg-contain bg-right-top md:flex-row"
       }
     >
       <img
         src={logoAnimal}
         alt="Logo du Bar"
-        className="w-2/3 items-center justify-center  p-20 sm:max-w-md md:max-w-md"
+        className="w-2/3 items-center justify-center p-20 sm:max-w-md md:max-w-md"
       />
-      {/* <img
-        // src={lionBanner}
-        alt="Visage de Lion"
-        className="w-1/4 md:max-w-md"
-      /> */}
+      <div>
+        <CallToAction icon="/path/to/icon.png" link="https://example.com" />
+        <CallToAction icon="/path/to/icon.png" link="https://example.com" />
+      </div>
     </div>
   )
 }
