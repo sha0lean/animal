0221b5cb57d8072a47cb81f28c5a7837400a8355
shao <ligeard.pro@gmail.com>
2023-12-01 01:27:15 -0500

    🔧  update config files

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  printWidth: 120,
+}
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,40 +5,36 @@ export default {
   theme: {
     // redéfinir complètement une propriété
     screens: {
-      sm:   "480px",
-      md:   "768px",
-      lg:   "976px",
-      xl:   "1440px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
 
     extend: {
       // ajouter ou modifier légèrement les paramètres par défaut.
       colors: {
         // bg-"alias" || text-"alias" || border-"alias" || etc...
-        blue:        "#1fb6ff",
-        pink:        "#EA80FC",
-        green:       "#13ce66",
-        gray:        "#8492a6",
-        graylight:   "#d3dce6",
-        graydark:    "#273444",
+        blue: "#1fb6ff",
+        pink: "#EA80FC",
       },
       backgroundColor: {
         // bg-"alias"
-        primary:     "#292929",
-        secondary:   "#fff0bf",
+        primary: "#1E1E1E",
+        secondary: "#fff0bf",
       },
       textColor: {
         // text-"alias"
-        primary:     "#f3f3f3",
-        secondary:   "#000000",
+        primary: "#f3f3f3",
+        secondary: "#000000",
       },
       fontFamily: {
         // font-"alias"
-        body:      ["Open Sans",  "sans-serif"],
-        heading:   ["Montserrat", "sans-serif"],
-        geist:     ["Geist",      "sans-serif"],
+        heading:  ["Italiana",  "sans-serif"],
+        body:     ["Open Sans", "sans-serif"],
+        special:  ["Geist",     "sans-serif"],
       },
+      plugins: [],
     },
-    plugins: [],
   },
 }
