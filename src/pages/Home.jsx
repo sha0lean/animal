c17b593778bacd808f896b2f66b6c8794fa0307e
shao <ligeard.pro@gmail.com>
2023-12-06 00:00:08 -0500

    ✨ add section text bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 //=components/pages/Home.jsx
-
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
 import Card from "../components/card/Card"
 import Hero from "../components/hero/Hero"
+import CallToAction from "../components/ui/CallToAction"
 import Breakpoints from "../components/utils/Breakpoints"
 
 const Home = () => {
@@ -31,9 +33,57 @@ const Home = () => {
             </p>
             <p className="text-sm md:text-2xl lg:text-3xl">Ville-la-Grand</p>
           </div>
-        </div>
-        <Breakpoints />
+        </div>{" "}
       </div>
+      <div className="flex flex-col items-center justify-between border-4 border-amber-400 bg-secondary text-justify font-heading text-secondary md:flex-row md:gap-14">
+        <div className="my-14 sm:mx-20 md:mx-5 md:w-1/2">
+          <p>
+            Plongez dans l'atmosphère vibrante de l'Animal Lodge Bar, où notre équipe
+            dévouée et passionnée oeuvre inlassablement pour créer une expérience
+            exceptionnelle pour nos clients... Guidée par une passion commune pour
+            l'hospitalité, chaque membre de notre équipe est déterminé à offrir un service
+            attentionné, dans une ambiance chaleureuse et amicale qui fait de notre
+            établissement un véritable bar d'ambiance...
+          </p>
+        </div>
+        <div className="space-y-4 sm:mx-20 md:mx-5 md:w-1/2">
+          <p>
+            Plongez dans l'atmosphère vibrante de l'Animal Lodge Bar, où notre équipe
+            dévouée et passionnée oeuvre inlassablement pour créer une expérience
+            exceptionnelle pour nos clients... Guidée par une passion commune pour
+            l'hospitalité, chaque membre de notre équipe est déterminé à offrir un service
+            attentionné, dans une ambiance chaleureuse et amicale qui fait de notre
+            établissement un véritable bar d'ambiance...
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center border-2 border-amber-800 bg-secondary p-8 md:flex-row md:gap-14 md:p-16">
+        <div className="flex justify-center space-x-4">
+          <span className="block h-8 w-8 rounded-full bg-primary"></span>
+          <span className="block h-8 w-8 rounded-full bg-primary"></span>
+        </div>
+        <div>
+          <p className="pl-8 text-center font-heading text-secondary">à propos link</p>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center border-4 border-amber-300 bg-secondary p-8 md:flex-row md:gap-14 md:p-16">
+        <div id="CTA" className="my-auto flex w-1/6 flex-col items-end gap-6">
+          <CallToAction
+            icon={faPhone}
+            altText="Contact par téléphone"
+            link="/Telephone"
+            iconSize="2x"
+          />
+          <CallToAction
+            icon={faWhatsapp}
+            altText="Contact par Whatsapp"
+            link="/Whatsapp"
+            iconSize="3x"
+          />
+        </div>
+      </div>
+      <Breakpoints />
     </div>
   )
 }
