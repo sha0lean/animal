4478e723024c07da2b4e5f997c3e7adc0faefd36
shao <ligeard.pro@gmail.com>
2023-12-08 01:35:20 -0500

    ✨ new Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ import Card from "../components/card/Card"
 import Hero from "../components/hero/Hero"
 import Hippo from "../components/hippo/Hippo"
 import CallToAction from "../components/ui/CallToAction"
-import Breakpoints from "../components/utils/Breakpoints"
 
 const Home = () => {
   return (
@@ -14,7 +13,7 @@ const Home = () => {
       <Hero />
       <div
         id="Cards"
-        className="container mx-auto mb-14 grid grid-cols-1 gap-14 px-4 sm:px-6 md:grid-cols-2 md:gap-10 lg:max-w-6xl lg:grid-cols-4 lg:px-8"
+        className="container mx-auto mb-14 grid grid-cols-1 gap-14 px-4 py-16 sm:px-6 md:grid-cols-2 md:gap-10 lg:max-w-6xl lg:grid-cols-4 lg:px-8"
       >
         <Card imageUrl="./src/assets/cards/card_lieu.webp" title="lieu" />
         <Card imageUrl="./src/assets/cards/card_cocktail.webp" title="cocktails" />
@@ -78,7 +77,6 @@ const Home = () => {
       </div>
 
       <div className="flex flex-row items-center justify-center bg-secondary"></div>
-      <Breakpoints />
     </div>
   )
 }
