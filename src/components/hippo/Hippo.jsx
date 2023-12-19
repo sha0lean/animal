85fadaab5e3394b1147b6e2fcc5cc1e0b3386449
sha0lean <ligeard.pro@gmail.com>
2023-12-19 21:15:04 +0100

    ✨ Refactor Hippo component to use imported image

--- src/components/hippo/Hippo.jsx
+++ src/components/hippo/Hippo.jsx
@@ -1,11 +1,13 @@
 //=components/hippo/Hippo.jsx
 
+import hippoImage from "@assets/artwork/hippo.webp"
+
 const Hippo = () => {
   return (
     <div
       id="hippo"
       className="relative h-[240px] w-full bg-[size:100%] bg-top bg-no-repeat sm:h-[360px] md:h-[510px] lg:h-[650px]"
-      style={{ backgroundImage: `url('/src/assets/artwork/hippo.webp')` }}
+      style={{ backgroundImage: `url(${hippoImage})` }}
     >
       <div className="flex h-full items-start justify-end pr-10 font-heading">
         <div className="pt-8 text-right text-white md:p-16 md:pt-28">
