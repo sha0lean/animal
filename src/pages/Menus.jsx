ea0d1ee3a1b83fd80e08153de5a2af6b0ca1ee1b
shao <ligeard.pro@gmail.com>
2024-01-16 21:58:24 +0100

    ✨ add section "Régalez-vous" w/ parallax effect lib

--- src/pages/Menus.jsx
+++ src/pages/Menus.jsx
@@ -1,13 +1,14 @@
 //=components/pages/Menus.jsx
 
 import imgRegalezVous from "@assets/cocktails/cocktail4.webp"
+import { Parallax } from "react-parallax"
 
 const Menus = () => {
   return (
-    <section className="flex flex-col  md:flex-row">
-      <div className="b1 flex-1 bg-opacity-50 p-8 text-white">
-        <h2 className="b2 mb-4 font-heading text-4xl font-bold">régalez-vous !</h2>
-        <p className="mb-4 font-body">
+    <section className="flex flex-col  pb-96 md:flex-row md:pt-10">
+      <div className="md:min-w-40 flex  flex-col justify-center bg-opacity-50 p-8 text-white md:mx-auto md:w-2/3 md:px-20 lg:px-40">
+        <h2 className="mb-4 pb-6 font-heading text-5xl font-bold ">régalez-vous !</h2>
+        <p className="mb-4 text-justify font-body xl:text-xl">
           Découvrez l&apos;excellence du plaisir gustatif à l&apos;Animal Lodge Bar, un
           établissement idéal pour déguster des cocktails raffinés et savourer des tapas
           entre amis dans le cadre exotique de la Haute-Savoie. Nos menus de cocktails et
@@ -18,12 +19,20 @@ const Menus = () => {
           préférences.
         </p>
       </div>
-      <div className="flex-1">
+      <div className="flex md:w-1/3">
         <img
           src={imgRegalezVous}
           alt="Animal Lodge Bar"
-          className="h-full w-full object-cover"
+          className="fixed-image h-full w-full object-cover"
         />
+        <Parallax
+          bgImage={imgRegalezVous}
+          bgImageAlt="Cocktail Chocolaté"
+          strength={-90}
+          className="parallax-image h-full w-full object-bottom"
+        >
+          <div style={{ height: 400 }} />
+        </Parallax>
       </div>
     </section>
   )
